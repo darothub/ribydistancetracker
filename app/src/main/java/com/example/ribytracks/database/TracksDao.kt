ffed8cdaed4416000e3fed7951404f2303cd7c90
lepea <darotudeen@gmail.com>
2020-01-14 21:23:09 +0100

    created DAO interface

--- app/src/main/java/com/example/ribytracks/database/TracksDao.kt
+++ app/src/main/java/com/example/ribytracks/database/TracksDao.kt
@@ -1,4 +1,22 @@
 package com.example.ribytracks.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
 interface TracksDao {
+
+    @Insert
+    suspend fun insert(tracksEntity: TracksEntity)
+
+    @Update
+    suspend fun update(tracksEntity: TracksEntity)
+
+    @get:Query(
+        "SELECT * FROM tracksentity ORDER BY id DESC"
+    )
+    val allTracks: LiveData<List<TracksEntity?>?>?
 }
