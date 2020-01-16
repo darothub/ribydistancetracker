361166b71bacaf5db2afebd9d32b61678bb1e533
lepea <darotudeen@gmail.com>
2020-01-16 12:43:26 +0100

    connect database and view model

--- app/src/main/java/com/example/ribytracks/viewmodel/TracksViewModel.kt
+++ app/src/main/java/com/example/ribytracks/viewmodel/TracksViewModel.kt
@@ -2,22 +2,27 @@ package com.example.ribytracks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import com.example.ribytracks.database.TracksEntity
-//import com.example.ribytracks.database.TracksRepository
+import com.example.ribytracks.database.TracksRepository
 
-//class TracksViewModel(application: Application):AndroidViewModel(application) {
-//    private var repository = TracksRepository(application)
-//    private var allTracks = repository.allTracks
-//
-//    fun insert(tracksEntity: TracksEntity, application: Application){
-//        repository.insert(tracksEntity, application)
-//    }
-//
-//    fun update(tracksEntity: TracksEntity, application: Application){
-//        repository.update(tracksEntity, application)
-//    }
-//
-//    fun deleteAll(application: Application){
-//        repository.deleteAll(application)
-//    }
-//}
+class TracksViewModel(application: Application):AndroidViewModel(application) {
+    private var repository = TracksRepository(application)
+    private var allTracks = repository.allTracks
+
+    fun getAllTracks():LiveData<List<TracksEntity?>?>?{
+        return allTracks
+    }
+
+    fun insert(tracksEntity: TracksEntity, application: Application){
+        repository.insert(tracksEntity, application)
+    }
+
+    fun update(tracksEntity: TracksEntity, application: Application){
+        repository.update(tracksEntity, application)
+    }
+
+    fun deleteAll(application: Application){
+        repository.deleteAll(application)
+    }
+}
